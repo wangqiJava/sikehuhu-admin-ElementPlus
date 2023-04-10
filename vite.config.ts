b10146ceef1e76a026b900b10dc0bc8a25098712
wangqi <1445343267@qq.com>
2023-04-10 09:35:23 +0800

    构建: 📦️ init

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,23 @@
-import { defineConfig } from "vite";
+import { defineConfig, UserConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import eslintPlugin from "vite-plugin-eslint";
+import viteCompression from "vite-plugin-compression";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [vue(), eslintPlugin()]
+export default defineConfig((): UserConfig => {
+	return {
+		plugins: [
+			vue(),
+			// * EsLint 报错信息显示在浏览器界面上
+			eslintPlugin(),
+			// * gzip compress
+			viteCompression({
+				verbose: true,
+				disable: false,
+				threshold: 10240,
+				algorithm: "gzip",
+				ext: ".gz"
+			})
+		]
+	};
 });
