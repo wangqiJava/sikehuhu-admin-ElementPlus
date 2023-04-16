d201011468639d6cb538d996d0e23396c7c1ad4c
wangqi <1445343267@qq.com>
2023-04-17 00:03:31 +0800

    构建: 📦️ init
    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -15,6 +15,14 @@ const count = ref(0);
 			Edit
 			<code>components/HelloWorld.vue</code> to test HMR
 		</p>
+		<el-row class="mb-4">
+			<el-button>Default</el-button>
+			<el-button type="primary">Primary</el-button>
+			<el-button type="success">Success</el-button>
+			<el-button type="info">Info</el-button>
+			<el-button type="warning">Warning</el-button>
+			<el-button type="danger">Danger</el-button>
+		</el-row>
 	</div>
 
 	<p>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
+// element plus
+import ElementPlus from "element-plus";
+// element css
+import "element-plus/dist/index.css";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+
+app.use(ElementPlus).mount("#app");
